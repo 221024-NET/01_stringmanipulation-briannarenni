6e889bd0b2aa8dab80317468a39b82fbbe3d9582
Brianna Renni <briannaxrene@icloud.com>
2022-10-27 22:08:37 -0700

    Add variables for user input

--- StringManipulationChallenge/StringManipulationChallenge.cs
+++ StringManipulationChallenge/StringManipulationChallenge.cs
@@ -6,55 +6,61 @@ namespace StringManipulationChallenge
     {
         static void Main(string[] args)
         {
-            string userInputString; //this will hold your users message
-            int elementNum;         //this will hold the element number within the messsage that your user indicates
-            char char1;             //this will hold the char value that your user wants to search for in the message.
-            string fName;           //this will hold the users first name
-            string lName;           //this will hold the users last name
-            string userFullName;    //this will hold the users full name;
-            
-            //
-            //
-            //implement the required code here and within the methods below.
-            //
-            //
+
+            Console.WriteLine("Please enter your message and press Enter: ");
+            string userInputString = Console.ReadLine();
+
+            Console.WriteLine("Please enter a number LESS THAN the length of your string and press enter");
+            int elementNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            string userFullName = $"{firstName}{lastName}";
+
+            Console.WriteLine("For which character should I search in your original message?");
+            char userChar = Convert.ToChar(Console.ReadLine());
+
 
 
         }
 
-        // This method has one string parameter. 
+        // This method has one string parameter.
         // It will:
-        // 1) change the string to all upper case, 
-        // 2) print the result to the console and 
+        // 1) change the string to all upper case,
+        // 2) print the result to the console and
         // 3) return the new string.
         public static string StringToUpper(string x){
-            throw new NotImplementedException("StringToUpper method not implemented.");
+            return x.ToUpper();
         }
 
-        // This method has one string parameter. 
+        // This method has one string parameter.
         // It will:
-        // 1) change the string to all lower case, 
-        // 2) print the result to the console and 
-        // 3) return the new string.        
+        // 1) change the string to all lower case,
+        // 2) print the result to the console and
+        // 3) return the new string.
         public static string StringToLower(string x){
             throw new NotImplementedException("StringToUpper method not implemented.");
 
         }
-        
-        // This method has one string parameter. 
+
+        // This method has one string parameter.
         // It will:
-        // 1) trim the whitespace from before and after the string, 
-        // 2) print the result to the console and 
+        // 1) trim the whitespace from before and after the string,
+        // 2) print the result to the console and
         // 3) return the new string.
         public static string StringTrim(string x){
             throw new NotImplementedException("StringTrim method not implemented.");
 
         }
-        
-        // This method has two parameters, one string and one integer. 
+
+        // This method has two parameters, one string and one integer.
         // It will:
-        // 1) get the substring based on the integer received, 
-        // 2) print the result to the console and 
+        // 1) get the substring based on the integer received,
+        // 2) print the result to the console and
         // 3) return the new string.
         public static string StringSubstring(string x, int elementNum){
             throw new NotImplementedException("StringSubstring method not implemented.");
